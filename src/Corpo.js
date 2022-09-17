78ed137dff81a7a1ea872703a887a99cdde96047
Luiz Cláudio <lcfernandez@cos.ufrj.br>
2022-09-17 10:40:43 -0300

    Componentização dos demais elementos da página
    - Corpo;
    - Navbar;
    - FundoMobile;
    - Sidebar;
    - Stories;
    - Posts;
    - Usuario;
    - Sugestoes.

---
+++ src/Corpo.js
@@ -0,0 +1,16 @@
+import Sidebar from './Sidebar';
+import Stories from './Stories';
+import Posts from './Posts';
+
+export default function Corpo() {
+    return (
+        <div className="corpo">
+            <div className="esquerda">
+                <Stories />
+                <Posts />
+            </div>
+
+            <Sidebar />            
+        </div>
+    );
+}
