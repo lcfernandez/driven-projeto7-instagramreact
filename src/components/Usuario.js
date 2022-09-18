736eafabeb0fe10f182b7112124d37adbefdfb59
lcfernandez <lcfernandez@cos.ufrj.br>
2022-09-17 23:28:53 -0300

    Ação de alterar nome de usuário

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,12 +1,16 @@
+import React from 'react';
+
 export default function Usuario() {
+    const [nome, setNome] = React.useState("Catana");
+
     return (
         <div className="usuario">
-            <img alt="" src="assets/img/catanacomics.svg" />
+            <img alt="Foto de catanacomics" src="assets/img/catanacomics.svg" />
             <div className="texto">
                 <strong>catanacomics</strong>
                 <span>
-                    Catana
-                    <ion-icon name="pencil"></ion-icon>
+                    {nome}
+                    <ion-icon name="pencil" onClick={() => setNome(prompt("Deseja alterar o seu nome usuário para qual nome?"))}></ion-icon>
                 </span>
             </div>
         </div>
