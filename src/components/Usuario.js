f01bb4e2894ac130f5c9d24f158961818dc671e9
lcfernandez <lcfernandez@cos.ufrj.br>
2022-09-17 23:35:13 -0300

    Ação de alterar imagem de perfil

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 export default function Usuario() {
     const [nome, setNome] = React.useState("Catana");
+    const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg");
 
     return (
         <div className="usuario">
-            <img alt="Foto de catanacomics" src="assets/img/catanacomics.svg" />
+            <img alt="Foto de catanacomics" src={imagem} onClick={() => setImagem(prompt("Deseja alterar a sua imagem de perfil com qual link?"))}/>
             <div className="texto">
                 <strong>catanacomics</strong>
                 <span>
