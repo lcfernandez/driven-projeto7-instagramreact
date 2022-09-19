a7961601393da1ff9f484f99b27817469d4380c0
lcfernandez <lcfernandez@cos.ufrj.br>
2022-09-19 02:05:21 -0300

    Validações para nome de usuário e imagem de perfil

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,17 +1,46 @@
 import React from 'react';
 
 export default function Usuario() {
+    const validateURL = (url) => {
+        let test;
+    
+        try {
+            test = new URL(url);
+        } catch (_) {
+            return false;
+        }
+    
+        return test.protocol === "http:" || test.protocol === "https:";
+    }
+
     const [nome, setNome] = React.useState("Catana");
     const [imagem, setImagem] = React.useState("assets/img/catanacomics.svg");
 
     return (
         <div className="usuario">
-            <img alt="Foto de catanacomics" src={imagem} onClick={() => setImagem(prompt("Deseja alterar a sua imagem de perfil com qual link?"))}/>
+            <img alt="Foto de catanacomics" src={imagem} onClick={
+                () => {
+                    let novaImagem;
+                    do {
+                        novaImagem = prompt("Qual é o link para sua nova imagem de perfil? (use 'http:' ou 'https:')")
+                    } while (
+                        !validateURL(novaImagem)
+                    );
+                    setImagem(novaImagem)
+                }
+            }/>
             <div className="texto">
                 <strong>catanacomics</strong>
                 <span>
                     {nome}
-                    <ion-icon name="pencil" onClick={() => setNome(prompt("Deseja alterar o seu nome usuário para qual nome?"))}></ion-icon>
+                    <ion-icon name="pencil" onClick={
+                        () => {
+                            let novoNome;
+                            do {novoNome = prompt("Qual será o seu novo nome de usuário?")} while (novoNome === null || !novoNome.trim());
+                            setNome(novoNome)
+                        }
+                    }>
+                    </ion-icon>
                 </span>
             </div>
         </div>
